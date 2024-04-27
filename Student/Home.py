69a70f22370ea776f5f7201a1c978ec80aef3d07
ozensei <sotelo.armand@gmail.com>
2024-04-27 20:32:33 +0800

    osen - updated home page for profile side_bar

--- Student/Home.py
+++ Student/Home.py
@@ -4,6 +4,16 @@ from streamlit_gsheets import GSheetsConnection
 from st_pages import Page, Section,show_pages, add_page_title
 from streamlit_extras.switch_page_button import switch_page
 import control_flow as cf
+from PIL import Image
+
+global name
+global mood
+global experience
+
+# parameters for now, this will come from survey page:
+mood = 'Happy'
+name = 'Osen'
+experience = 999
 
 # Initialize
 st.set_page_config(page_title='[NAME] Students\' Portal', page_icon='🌱', layout="centered", initial_sidebar_state="auto", menu_items=None)
@@ -54,3 +64,50 @@ def check_password():
 
 if not check_password():
     st.stop()
+
+# Profile at bottom left of sidebar
+def profile_sidebar():
+    def image_to_base64(image):
+        import base64
+        import io
+        buffered = io.BytesIO()
+        image.save(buffered, format="PNG")
+        return base64.b64encode(buffered.getvalue()).decode()
+
+    background_image = Image.open("images/background.png")
+    if mood == 'Happy':
+        cat_image = Image.open("images/cat_happy.png")
+    elif mood == 'Amused':
+        cat_image = Image.open("images/cat_amused.png")
+    elif mood == 'Inspired':
+        cat_image = Image.open("images/ccat_inspired.png")
+    elif mood == 'Dont Care':
+        cat_image = Image.open("images/ccat_dont_care.png")
+    elif mood == 'Annoyed':
+        cat_image = Image.open("images/ccat_annoyed.png")
+    elif mood == 'Afraid':
+        cat_image = Image.open("images/ccat_afraid.png")
+    elif mood == 'Sad':
+        cat_image = Image.open("images/ccat_sad.png")
+    elif mood == 'Angry':
+        cat_image = Image.open("images/ccat_angry.png")
+
+
+    # Calculate level and remaining experience
+    level = experience // 100
+    remaining_experience = experience % 100
+
+    # Calculate width of level bar based on remaining experience
+    level_bar_width = remaining_experience / 100 * 150  # Percentage of 100 pixels
+
+    # Display stacked images with text
+    with st.sidebar:
+        st.write(
+        f"""
+        <div style="position:relative;">
+            <img src="data:image/png;base64,{image_to_base64(background_image)}" style="width:100%;">
+            <img src="data:image/png;base64,{image_to_base64(cat_image)}" style="position:absolute;top:50%;left:50%;transform:translate(-50%, -50%);width:75%">
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
