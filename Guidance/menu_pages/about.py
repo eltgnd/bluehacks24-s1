1f273bfea4556b102d6cfbfd0d33ca54f778b34e
Miguel Antonio H. Germar <migs.germar@gmail.com>
2024-04-28 11:37:02 +0800

    migs-update About pages

--- Guidance/menu_pages/about.py
+++ Guidance/menu_pages/about.py
@@ -15,23 +15,39 @@ if __name__ == "__main__":
     # Load initial data if it hasn't already been loaded.
     cf.load_initial_data_if_needed()
 
-    st.markdown("(PROJECT TITLE)")
+    st.caption('BUGHAW   |   STUDENTS\' PORTAL')
     st.title(f"{emoji} About")
 
-    st.markdown("""The app was developed by...
-
-The following are our sources...
-
-                """)
-    
-    st.markdown("---")
-
-    st.markdown("""Contact Information
-    
-- Email: ...
-- GitHub: ...
-- LinkedIn: ...
+    st.markdown("""The Bughaw web app was developed by 2nd year BS Applied Mathematics - Master in Data Science students in Ateneo de Manila University.
                 
-    """)
+- Miguel Antonio "Migs" Germar
+- Val Allen "Val" Eltagonde
+- Armand "Osen" Sotelo
+- Aames Juriel "Aames" Morales
+- Franz Andrei "Franz" Layug
+
+We developed this app mainly by referring to the documentation of Streamlit, an open source Python package for developing web apps (Snowflake Inc. 2024). For the Mental Health Feed, we used the publicly available code provided by Muralidhar (2023) for scraping articles from the RSS feeds of websites. Also, we referred to Alibudbud (2021) for two simple surveys that would holistically assess a student's mental health status.
+                
+---
+                
+**Reference list**
+                
+Alibudbud, R. (2021). Academic Experiences as Determinants of Anxiety and Depression of Filipino College Students in Metro Manila. Youth Voice Journal, 11, 1–21.
+                
+Muralidhar, K. S. V. (2023, July 7). How to Build an RSS feed-based News app with Streamlit, Heroku and Android Studio? Medium. https://ksvmuralidhar.medium.com/how-to-build-an-rss-feed-based-news-app-with-streamlit-heroku-and-android-studio-cc4ebb1077e
+
+Snowflake Inc. (2024). API Reference. Streamlit Docs. https://docs.streamlit.io/
+
+""")
+    
+#     st.markdown("---")
+
+#     st.markdown("""Contact Information
+    
+# - Email: ...
+# - GitHub: ...
+# - LinkedIn: ...
+                
+#     """)
     
     cf.display_copyright()
